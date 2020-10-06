d465311352a1db96c06df66eedf2aee541c28fbb
whendev <whendel.morais.9@gmail.com>
2020-10-06 13:59:52 -0300

    Resolvido

--- src/main/java/com/example/project/Loja.java
+++ src/main/java/com/example/project/Loja.java
@@ -47,9 +47,84 @@ public class Loja {
         this.endereco = endereco;
     }
 
+    public static Boolean isNullOrEmpty(String s) {
+        return s == null || s.isEmpty();
+    }
+
+    public void validarCamposObrigatorio(){
+        if (isNullOrEmpty(this.getNomeLoja())){
+            throw new RuntimeException("O campo nome da loja é obrigatório");
+        }
+
+        if (isNullOrEmpty(endereco.getLogradouro())){
+            throw new RuntimeException("O campo logradouro do endereço é obrigatório");
+        }
+
+        if (isNullOrEmpty(endereco.getMunicipio())){
+            throw new RuntimeException("O campo município do endereço é obrigatório");
+        }
+
+        if (isNullOrEmpty(endereco.getEstado())){
+            throw new RuntimeException("O campo estado do endereço é obrigatório");
+        }
+
+        if (isNullOrEmpty(this.getCnpj())){
+            throw new RuntimeException("O campo cnpj da loja é obrigatório");
+        }
+
+        if (isNullOrEmpty(this.getInscricaoEstadual())) {
+            throw new RuntimeException("O campo inscrição estadual da loja é obrigatório");
+        }
+    }
+
+
     public String dadosLoja() {
 		// Implemente aqui
-		return null;
-	}
+
+        validarCamposObrigatorio();
+        String _COMPLEMENTO = "";
+        if (!isNullOrEmpty(endereco.getComplemento())){
+            _COMPLEMENTO = " " + endereco.getComplemento();
+        }
+
+        String _BAIRRO = "";
+        if (!isNullOrEmpty(endereco.getBairro())) {
+            _BAIRRO = endereco.getBairro() + " - ";
+        }
+
+        String _CEP = "";
+        String _TELEFONE = "";
+
+        if (!isNullOrEmpty(endereco.getCep())) {
+            _CEP = "CEP:" + endereco.getCep();
+            if (!isNullOrEmpty(this.getTelefone())){
+                _TELEFONE = " Tel " + this.getTelefone();
+            }
+        } else {
+            _CEP = "";
+            if (!isNullOrEmpty(this.getTelefone())){
+                _TELEFONE = "Tel " + this.getTelefone();
+            }
+        }
+
+        String _OBSERVACAO = "";
+        if (!isNullOrEmpty(this.getObservacao())){
+            _OBSERVACAO = this.getObservacao();
+        }
+
+        String _texto = "";
+        _texto = String.format("%s\r\n",this.getNomeLoja());
+        if (endereco.getNumero() == 0){
+            _texto += String.format("%s, %s%s\r\n",endereco.getLogradouro(),"s/n",_COMPLEMENTO);
+        } else {
+            _texto += String.format("%s, %d%s\r\n",endereco.getLogradouro(),endereco.getNumero(),_COMPLEMENTO);
+        }
+        _texto += String.format("%s%s - %s\r\n",_BAIRRO,endereco.getMunicipio(),endereco.getEstado());
+        _texto += String.format("%s%s\r\n",_CEP,_TELEFONE);
+        _texto += String.format("%s\r\n",_OBSERVACAO);
+        _texto += String.format("CNPJ: %s\r\n",this.getCnpj());
+        _texto += String.format("IE: %s\r\n",this.getInscricaoEstadual());
+        return _texto;
+    }
 
 }
